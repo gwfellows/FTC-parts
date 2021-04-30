eedbb40d48fc1a65d3483a4b4d87c4ada11c1c82
grantfellows <gwfellows@icloud.com>
2021-04-29 23:04:06 -0700

    reorganize

---
+++ get_parts.py
@@ -0,0 +1,47 @@
+from bs4 import BeautifulSoup
+import requests
+import zipfile
+import os
+
+
+def get_goBILDA_parts():
+    sitemap_page = requests.get(
+        'https://www.gobilda.com/sitemap/categories/', allow_redirects=True)
+    section_links = (str(a['href']) for a in BeautifulSoup(sitemap_page.content, 'html.parser').find(
+        'div', class_='container').find_all('a'))
+
+    for section_link in section_links:
+        section_page = requests.get(section_link, allow_redirects=True)
+        part_links = (str(a['href']) for a in BeautifulSoup(
+            section_page.content, 'html.parser').find_all('a', class_='card'))
+
+        for part_link in part_links:
+            if part_link[0] == '/':
+                part_link = 'https://www.gobilda.com'+part_link
+            part_page = requests.get(part_link, allow_redirects=True)
+            step_links = (str(a['href']) for a in BeautifulSoup(
+                part_page.content, 'html.parser').find_all('a', class_='product-downloadsList-listItem-link ext-zip'))
+
+            for step_link in step_links:
+                filename = "./"+step_link.replace('/content/step_files/', '')
+                print("Processing "+filename+"...")
+                with open(filename, 'wb') as file:
+                    file.write(requests.get(
+                        'https://www.gobilda.com'+step_link).content)
+                if zipfile.is_zipfile(filename):
+                    with zipfile.ZipFile(filename, "r") as to_unzip:
+                        if len(to_unzip.namelist()) > 1:
+                            to_unzip.close()
+                            os.remove(filename)
+                            print("Done\n")
+                            break
+                        to_unzip.extractall()
+                else:
+                    os.remove(filename)
+                    print("Done\n")
+                    break
+                os.remove(filename)
+                print("Done\n")
+
+
+def get_goBILDA_parts()
